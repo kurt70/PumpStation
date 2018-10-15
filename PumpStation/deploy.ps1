8682fe1d0d928010b9d5e43c43cb4b23669178c8
Kurt Monge <kurt.monge@gmail.com>
2018-10-15 23:15:30 +0200

    Misc

--- PumpStation/deploy.ps1
+++ PumpStation/deploy.ps1
@@ -1,5 +1,5 @@
 ﻿cd C:\Users\Admin\Source\Repos\PumpStation\PumpStation
 dotnet publish -r linux-arm
-cd C:\Users\Admin\Source\Repos\PumpStation\PumpStation\bin\Debug\netcoreapp2.0\linux-arm\publish
-psftp -pw bsdit001 pi@10.0.0.195 -b C:\Users\Admin\Source\Repos\PumpStation\PumpStation\transfercmds.txt -bc
+rem cd C:\Users\Admin\Source\Repos\PumpStation\PumpStation\bin\Debug\netcoreapp2.0\linux-arm\publish
+rem psftp -pw bsdit001 pi@10.0.0.195 -b C:\Users\Admin\Source\Repos\PumpStation\PumpStation\transfercmds.txt -bc
 cd C:\Users\Admin\Source\Repos\PumpStation\PumpStation
