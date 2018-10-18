41f627276c0301104a688e6e019749376c731473
Kurt Monge <kurt.monge@gmail.com>
2018-10-18 19:28:33 +0200

    Some miner changes in the docker buildscript

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,8 @@
-﻿docker build -t kurtmonge/pumpstation:4 .
-docker push kurtmonge/pumpstation:4
+﻿Param(
+    [string]$buildNumber
+)
+docker build -t kurtmonge/pumpstation:$buildNumber .
+docker push kurtmonge/pumpstation:$buildNumber
 Echo "Use the following on the host system:"
-Echo "docker pull kurtmonge/pumpstation:4"
-Echo "docker run -p 8888:5000 kurtmonge/pumpstation:4"
+Echo "docker pull kurtmonge/pumpstation:$buildNumber"
+Echo "docker run -p 8888:5000 kurtmonge/pumpstation:$buildNumber"
